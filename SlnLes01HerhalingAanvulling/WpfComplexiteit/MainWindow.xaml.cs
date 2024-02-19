6db4316606b8a54e1a7a12fc8035c69b1c609d5e
kurtis <kurtis.courthiau@student.odisee.be>
2024-02-20 00:41:19 +0100

    test

--- SlnLes01HerhalingAanvulling/WpfComplexiteit/MainWindow.xaml.cs
+++ SlnLes01HerhalingAanvulling/WpfComplexiteit/MainWindow.xaml.cs
@@ -20,18 +20,93 @@ namespace WpfComplexiteit
     /// </summary>
     public partial class MainWindow : Window
     {
+        string woord;
+        bool Klinker = false;
+        bool IsHetEenLetter = false;
+        int karakters = 0;
+        int Lettergrepen = 0;
+        char letter;
         public MainWindow()
         {
+            
             InitializeComponent();
-            string woord;
+            
 
-        }
+    }
 
         private void Analyseerbtn_Click(object sender, RoutedEventArgs e)
         {
+            woord = woordtbx.Text;
+            IsKlinker(letter);
+            IsLetter(woord);
+            AantalKarakters(woord);
+            AantalLettergrepen(woord);
+            complexiteitlbl.Content = "aantal karakters: " + AantalKarakters(woord) + " aantal lettergrepen: " + AantalLettergrepen(woord);
 
         }
 
-        private int 
+        public bool IsKlinker(char letter)
+        {
+            
+                switch (Convert.ToString(letter))
+                {
+                    case "a":
+                    case "e":
+                    case "i":
+                    case "o":
+                    case "u":
+                    case "y":
+                        Klinker = true; break;
+                    default: break;
+                }
+            return Klinker;
+            
+        }
+
+        public void IsLetter(string woord)
+        {
+            foreach (char karakter in woord)
+            {
+
+
+                if (((karakter<='Z') && (karakter>='A')) || ((karakter >= 'a') && (karakter <= 'z')))
+                {
+                    IsHetEenLetter = true;
+                    return;
+                }
+                
+            }
+        }
+
+        public int AantalKarakters(string woord)
+        {
+            int karakters = 0;
+            foreach (char letter in woord)
+            {
+                karakters++;
+            }
+            return karakters;
+        }
+
+        public int AantalLettergrepen(string woord)
+        {
+            int index = 0;
+            string woord2 = "w" + woord;
+            foreach (char letter1 in woord)
+            {
+
+                char letter2 = woord2[index];
+                if (IsKlinker(letter1) == false && IsKlinker(letter2) == true)
+                {
+                    Lettergrepen++;
+                }
+                index++;
+            }
+
+            return Lettergrepen;
+        }
+
+
+
     }
 }
